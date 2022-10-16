73fedf1e296ebe6caac3058f9aa584b2c5bff8d9
Burak Ozcan <burak.ozcan.g@hotmail.com>
2022-10-16 14:48:31 +0300

    default

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View , Text} from 'react-native';
+
+function App(){
+    return(
+      <View>
+        <Text>Selam</Text>
+      </View>
+    )
+}
+
+export default App;
